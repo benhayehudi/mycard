3c3f63a1f6bfa2c6af2e9311ceaf03d63e44ab0d
benhayehudi <bengreenberg@gmail.com>
2018-12-27 18:21:53 +0200

    added details json, began presentation methods

--- lib/mycard.rb
+++ lib/mycard.rb
@@ -1,6 +1,30 @@
-require "mycard/version"
+require "./lib/mycard/version.rb"
+require 'pry'
 
 module Mycard
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Card
+
+    attr_accessor :name, :job_title, :company, :twitter, :linkedin, :github, :portfolio, :gem
+
+    def initialize(details)
+      @name = details["name"]
+      @job_title = details["job_title"]
+      @company = details["company"]
+      @twitter = details["twitter"]
+      @linkedin = details["linkedin"]
+      @github = details["github"]
+      @portfolio = details["portfolio"]
+      @gem = details["gem"]
+    end
+
+    def call
+      display_card
+    end
+
+    def display_card
+      puts "my name is #{@name}"
+    end
+  end
 end
